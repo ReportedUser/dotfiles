1d6322f45b50f4ac99abf4887c0a4c9c79159458
ReportedUser <pleasefirsttry@gmail.com>
2025-05-10 20:11:11 +0200

    Add: pluggins
    Colorscheme and gitstatus

--- nvim/init.lua
+++
@@ -1,2 +0,0 @@
-require("zephyros")
-print("Hello")
--- nvim/lua/zephyros/lazy.lua
+++ nvim/lua/zephyros/lazy.lua
@@ -22,12 +22,6 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- import your plugins
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-})
+    		{ import = "plugins" },
+	}
+)
