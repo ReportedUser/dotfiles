dd0b318ef90d771db489b1ea064cc0731afb5ad2
ReportedUser <pleasefirsttry@gmail.com>
2025-05-14 15:51:20 +0200

    Change: some shit

--- nvim/lua/zephyros/lazy.lua
+++ nvim/lua/zephyros/lazy.lua
@@ -16,32 +16,35 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 
+
+
+
+
 -- Setup lazy.nvim
 require("lazy").setup({
     		{
 		import = "plugins", 
-		'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+		-- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
  		},
 	},
 	{
 	ui = {
-	    -- If you are using a Nerd Font: set icons to an empty table which will use the
-	    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-	    icons = vim.g.have_nerd_font and {} or {
-	      cmd = '⌘',
-	      config = '🛠',
-	      event = '📅',
-	      ft = '📂',
-	      init = '⚙',
-	      keys = '🗝',
-	      plugin = '🔌',
-	      runtime = '💻',
-	      require = '🌙',
-	      source = '📄',
-	      start = '🚀',
-	      task = '📌',
-	      lazy = '💤 ',
-	    },
-	  },
-	}
-)
+		-- If you are using a Nerd Font: set icons to an empty table which will use the
+		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
+})
